7c0e47a410e389389cd9679a54c2b70769814827
Vaibhav <vmdhoke@gmail.com>
2018-06-03 22:43:37 +0530

    Added Example Resume Format Changed route to post Removed Links in Index Pages Added Flash Messages with close button in Layout Added rescue block

--- app/controllers/resume_parser_controller.rb
+++ app/controllers/resume_parser_controller.rb
@@ -1,13 +1,18 @@
 class ResumeParserController < ApplicationController
 
-	def index
 
-	end
-
-	def parser
-		# file = params[:file]
-		# @read_file_contents = JSON.parse file.tempfile.read
-		@read_file_contents = JSON.parse File.read("resume.json")
-	end
+  def parser
+    file = params[:file]
+    if file.content_type != 'application/json'
+      flash[:danger] = 'Only JSON file type supported'
+      redirect_to root_path and return
+    end
+    begin
+      @read_file_contents = JSON.parse file.tempfile.read
+    rescue => e
+      flash[:danger] = "Could Not Parse File, Error #{e}"
+      redirect_to root_path
+    end
+  end
 
 end
