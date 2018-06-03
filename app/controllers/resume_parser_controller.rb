dd42055ea10dae5a88ace111ac8c4e9c224ce514
Vaibhav <vmdhoke@gmail.com>
2018-06-03 18:12:24 +0530

    Added Bootstrap

--- app/controllers/resume_parser_controller.rb
+++ app/controllers/resume_parser_controller.rb
@@ -1,11 +1,13 @@
 class ResumeParserController < ApplicationController
 
 	def index
-	
+
 	end
 
 	def parser
-		binding.pry
+		# file = params[:file]
+		# @read_file_contents = JSON.parse file.tempfile.read
+		@read_file_contents = JSON.parse File.read("resume.json")
 	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
 	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 	root to: 'resume_parser#index'
-	post '/parse' =>  'resume_parser#parser'
+	get '/parse' =>  'resume_parser#parser'
 
 end
